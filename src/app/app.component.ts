642a2362b4e4f02f691c109ccd0424044e99209f
Rahul Parihar <rahulkumarpariharmailbox@gmail.com>
2019-06-21 19:01:48 +0530

    Adding product-list.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: "pm-root",
-  template: "<div><h1>{{pageTitle}}</h1><div>My First Component</div></div>"
+  selector: 'pm-root',
+  template: '<div><h1>{{pageTitle}}</h1><pm-product></pm-product></div>'
 })
 
 export class AppComponent {
-  pageTitle: string = "Acme Product Management";
+  pageTitle = 'Acme Product Management';
 }

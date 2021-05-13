785b728aacc3814fc96d27ca67e14823556b5273
Rahul Kumar Parihar <rahulkumarpariharmailbox@gmail.com>
2021-05-13 10:28:57 +0530

    Modue 1 changes

---
+++ src/app/messages/message.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { SharedModule } from "../shared/shared.module";
+import { MessageComponent } from "./message.component";
+
+@NgModule({
+  imports: [SharedModule],
+  declarations: [MessageComponent],
+})
+export class MessageModule {}
